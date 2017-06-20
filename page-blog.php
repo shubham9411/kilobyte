4c0392b70c062d436b2079d4b238b56e5a901dbf
umeshdhauni <umeshdhauni@gmail.com>
2017-06-20 11:44:31 +0530

    Page

--- page-blog.php
+++ page-blog.php
@@ -5,26 +5,29 @@ Template Name: blog
 */
 ?>
 <div class="container">
-<div class="row">
-<?php
-$args = array('post_type' => 'post' );
-$loop = new WP_Query($args);
-if($loop->have_posts()):
-	while ($loop->have_posts()): $loop->the_post(); ?>
-	<div class="card col-sm-4 blogc">
-	<div class="thumbnail"><?php if(has_post_thumbnail() ){ ?>
-	<?php the_post_thumbnail('large','class= img-responsive');
-	}
+	<div class="row">
+	<?php
+	$args = array('post_type' => 'post' );
+	$loop = new WP_Query($args);
+	if($loop->have_posts()):
+		while ($loop->have_posts()): $loop->the_post(); ?>
+		<div class="card col-md-4 col-sm-6 col-xs-12 blogc">
+			<div class="thumbnail"><?php if(has_post_thumbnail() ){ ?>
+			<?php the_post_thumbnail('large','class= img-responsive');
+			}
+			?>
+			</div>
+			<div class="blog-detail">
+				<h1><?php the_title();?></h1>
+				<?php the_excerpt();?>
+			</div>
+		</div>
+		<?php
+		endwhile;
+	endif;
+	wp_reset_postdata();
 	?>
 	</div>
-	<h1><?php the_title();?></h1>
-	<?php the_excerpt();?>
-	</div>
-	<?php
-	endwhile;
-endif;
-wp_reset_postdata();
-?>
 </div>
 <?php 
 get_footer(); 
