36609b3ad7ed04ad77e2f9c50f6b7c6a8313b9ba
Shubham Pandey <shubham9411@gmail.com>
2017-06-20 11:17:43 +0530

    Portfolio and casestudy single & typos with naming conventions

--- page-casestudies.php
+++ page-casestudies.php
@@ -1,37 +1,38 @@
 <?php
-get_header(); 
-/* 
+get_header();
+/*
 Template Name: casestudies
 */
 get_header();
 ?>
 <div class="container">
-<?php
-$args = array('post_type' => 'casestudy' );
-$loop = new WP_Query($args);
-if($loop->have_posts()):
+	<?php
+	$args = array('post_type' => 'post-type-casestudy' );
+	$loop = new WP_Query($args);
+	if($loop->have_posts()):
 	while ($loop->have_posts()): $loop->the_post(); ?>
 	<div class="card">
-	<div class="row">
-		<div class="col-md-4 col-sm-5 case-study-image">
-			<div class="thumbnail"><?php if(has_post_thumbnail() ): ?>
-			<?php the_post_thumbnail('large','class=img-responsive');
-			endif;
-			?>
+		<div class="row" onclick="window.location = '<?php the_permalink(); ?>';">
+
+			<div class="col-md-4 col-sm-5 case-study-image">
+				<div class="thumbnail"><?php if(has_post_thumbnail() ): ?>
+					<?php the_post_thumbnail('large','class=img-responsive');
+					endif;
+					?>
+				</div>
+			</div>
+			<div class="col-md-8 col-sm-7 case-study-content">
+				<h1><?php the_title()?></h1>
+				<?php the_excerpt();?>
 			</div>
 		</div>
-		<div class="col-md-8 col-sm-7 case-study-content">
-		<h1><?php the_title()?></h1>
-		<?php 	the_excerpt();?>
-		</div>
-	</div>
 	</div><br>
-	<?php 
+	<?php
 	endwhile;
-endif;
-wp_reset_postdata();
-?>
+	endif;
+	wp_reset_postdata();
+	?>
 </div>
-<?php 
-get_footer(); 
+<?php
+get_footer();
 ?>
--- template-portfolio.php
+++ template-portfolio.php
@@ -9,7 +9,7 @@ get_header();
 <div class="container portfolio">
 <?php
 $args = array(
-	'post_type'   => 'portfolio',
+	'post_type'   => 'post-type-portfolio',
 );
 $loop = new WP_Query( $args );
 if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post();
@@ -17,11 +17,13 @@ if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post();
 	?>
 	<div class="col-xs-6">
 		<div class="wrap card">
+			<a href="<?php the_permalink();?>">
 			<?php the_post_thumbnail( 'portifolio-thumbnail', 'class=img-responsive' ); ?>
 			<div class="port-meta">
 				<h3 class="head"><?php the_title();?></h3>
 				<p class="bodytext"><?php echo get_post_meta( $post->ID, 'sub_heading', true ); ?></p>
 			</div>
+			</a>
 		</div>
 	</div>
 	<?php
