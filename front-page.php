7c93cb4e58611af6016977a8bba4920e9234fca8
Shubham Pandey <shubham9411@gmail.com>
2017-06-21 13:23:28 +0530

    Solving problems through tech

--- front-page.php
+++ front-page.php
@@ -1,35 +1,33 @@
 <?php
 /**
- * The landing page template
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package kilobyte
- */
-
+* The landing page template
+*
+* This is the most generic template file in a WordPress theme
+* and one of the two required files for a theme (the other being style.css).
+* It is used to display a page when nothing more specific matches a query.
+* E.g., it puts together the home page when no home.php file exists.
+*
+* @link https://codex.wordpress.org/Template_Hierarchy
+*
+* @package kilobyte
+*/
 get_header(); ?>
-	<div class="hero-part">
-		<div class="container">
+<div class="hero-part">
+	<div class="container">
 		<div class="row">
-		<div class="col-md-6 col-sm-6 col-xs-12">
-			<h1 class="tagline">TECH FOR YOUR <br>BUSINESS</h1>
-
-			<h3 class="short-intro">We are a digital agency that focused on solve problems through technology.
-			</h3>
-		<div class="start-btn">
-			<button class="input-lg">GET STARTED</button>
-		</div>
-		</div>
-		<div class="col-md-6 col-sm-6 col-xs-12 main-image">
-		<img src="<?php echo get_stylesheet_directory_uri().'/img/1.png';?>" class="img-responsive">
-		</div>
-		</div>
+			<div class="col-sm-6">
+				<h1 class="tagline">TECH FOR YOUR <br>BUSINESS</h1>
+				<h3 class="short-intro">We are a digital agency that focused on solving problems through technology.
+				</h3>
+				<div class="start-btn">
+					<button class="input-lg">GET STARTED</button>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<img src="<?php echo get_stylesheet_directory_uri().'/img/1.png';?>" class="img-responsive">
+			</div>
 		</div>
 	</div>
+</div>
 <?php
 get_footer();
