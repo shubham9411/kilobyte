08447cd53d0d9ce47126f4abfdec998f1133b689
dr4j <dhiraj.s.k5161@gmail.com>
2017-06-27 18:59:39 +0530

    changes added

--- contact-form.php
+++ contact-form.php
@@ -12,6 +12,13 @@ get_header();
 		jQuery("iframe").removeClass("clicked");	
 	}
 </script>
+<div class="container">
+<div class="contact-head">
+	<h1>LET'S CHAT</h1>
+	<p>Fill this out so we can learn more about you and your needs.</p>
+</div>
+
+
 <div class="contact">
 	<div class="contact-details">
 					<?php echo do_shortcode('[contact-form-7 id="102" title="Contact form 1"]'); ?>
@@ -19,7 +26,7 @@ get_header();
 		</div>
 
 <!-- map -->
-<div class="container row">
+<div class="row">
 	<div class="map">
 		<div class="col-md-6 col-sm-6 onclick="map()" onmouseleave="pointerRemove()">
 		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.6480567246886!2d77.3382219147242!3d28.640308382414727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfad3d093eddf%3A0xdd7e519ea30a44ae!2sKilobyte+Technologies!5e0!3m2!1sen!2sin!4v1498126995542" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>	
@@ -35,7 +42,7 @@ get_header();
 		</div>
 	</div>
 </div>
-
+</div>
 <br>
 </br>
 <?php
