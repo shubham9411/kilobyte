c41799b84edb9f33b6a352e67784e8d8a3628e56
dr4j <dhiraj.s.k5161@gmail.com>
2017-07-01 16:11:22 +0530

    changes

--- single-team-page.php
+++ single-team-page.php
@@ -13,7 +13,7 @@ get_header();
 							$author_info=get_userdata($_GET['uid']);
 						 echo $author_info->first_name . ' ' . $author_info->last_name;?>
 						 </h1>
-						<h3>(<?php echo $author_info->urole;?>)</h3>
+						<h3><?php if($author_info->urole) echo '('.$author_info->urole.')'?></h3>
 					</div>
 					<div class="col-md-5 col-sm-5">
 						<div ><div class="img-responsive"><?php echo get_avatar($author_info->ID, 220 );?></div>
@@ -27,15 +27,14 @@ get_header();
 				<div class="row">
 					<div class="col-md-7">
 						<div class="member-detailed">
-							<h2>DESCRIPTION</h2>
-							<p><?php  	echo $author_info->user_description;?></p>
+							<?php  	if($author_info->user_description){echo "<h2>DESCRIPTION</h2><p>".$author_info->user_description."</p>";}?>
 						</div>
 					</div>
 					<div class="col-md-5">
 						<div class="member-blog">
 							<h2>BLOGS</h2>
 								<?php
-								    $args = array( 'author__in'=>$authorID,'post_type'=> 'personalblogs');
+								    $args = array( 'author__in'=>$author_info->ID,'post_type'=> 'personalblogs');
 								    $loop = new WP_Query($args);
 								    if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post();
 								      ?><h2><?php the_title();?></h2>
