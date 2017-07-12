bae8411ff276c1997f73885b376dbd3dd8638dd9
umeshdhauni <umeshdhauni@gmail.com>
2017-07-12 22:02:10 +0530

    gif

--- front-page.php
+++ front-page.php
@@ -12,20 +12,32 @@
 * @package kilobyte
 */
 get_header(); ?>
+<script type="text/javascript">
+  jQuery(document).ready(function(){
+   var gifSource = jQuery('#image-gif').attr('src'); //get the source in the var
+    jQuery('#image-gif').attr('src', ""); //erase the source     
+    jQuery('#image-gif').attr('src', gifSource+"?"+new Date().getTime());
+  });
+</script>
 <div class="upper-z">
 <!-- hero section -->
 <div class="hero-section container">
-
-      <div class="tag-line">
-      Digital Products that Businesses <i>love.</i>
-      </div>
-      <div class="short-intro">
-        We craft digital products and experiences to start, scale and empower identities and organisations.
-      </div>
-      <div class="btn strt-btn">Experience More</div>
+    <div>
+        <div class="tag-line">
+            Digital Products that Businesses <i>love.</i>
+        </div>
+        <div class="short-intro">
+            We craft digital products and experiences to start, scale and empower identities and organisations.
+        </div>
+        <div class="btn strt-btn">Experience More</div>
+    </div>
+    <div class="gif row">
+        <div class="col-md-8 col-sm-6">
+            <img src="<?php echo get_stylesheet_directory_uri().'/img/main.gif';?>" class="img-responsive main-gif" id="image-gif">
+            <img src="<?php echo get_stylesheet_directory_uri().'/img/main.png';?>" class="img-responsive hero-image">
+        </div>
+    </div>
 </div>
-
-
 <!-- work section -->
 <?php
  $i=0;
@@ -44,27 +56,31 @@ get_header(); ?>
 <div class="work">
   <div class="work-meta container">
     <h1>Work</h1>
-        <div class="w1 port" style="background-image:url('<?php echo $thumb[0]["0"]?>');background-size: 100% 100%;">
-            <div class="port-meta">
-              <h2><?php echo $titl[0] ?></h2>
-              <p><?php echo $sub[0] ?></p>
+        <div class="row">
+          <div class="col-md-6 col-xs-12">
+            <div class="w1 port " style="background-image:url('<?php echo $thumb[0]["0"]?>');background-size: 100% 100%;">
+                <div class="port-meta">
+                  <h2><?php echo $titl[0] ?></h2>
+                  <p><?php echo $sub[0] ?></p>
+                </div>
+            </div>       
+            <div class="w2 port" style="background-image:url('<?php echo $thumb[1]["0"]?>');background-size: 100% 100%; ">
+                  <div class="port-meta">
+                    <h2><?php echo $titl[1] ?></h2>
+                    <p><?php echo $sub[1] ?></p>
+                  </div>
             </div>
-        </div>       
-        <div class="w2 port" style="background-image:url('<?php echo $thumb[1]["0"]?>');background-size: 100% 100%; ">
-              <div class="port-meta">
-                <h2><?php echo $titl[1] ?></h2>
-                <p><?php echo $sub[1] ?></p>
-              </div>
-        </div>
-      <div class="cont-2 row">
-          <div class="w3 port col-md-6" style="background-image:url('<?php echo $thumb[2]["0"]?>');background-size: 100% 100%; ">
+          </div>
+      <div class=" col-md-6 col-xs-12">
+          <div class="w3 port" style="background-image:url('<?php echo $thumb[2]["0"]?>');background-size: 100% 100%; ">
               <div class="port-meta">
                 <h2><?php echo $titl[2] ?></h2>
                 <p><?php echo $sub[2] ?></p>
               </div>
           </div>
-          <div class="sm col-md-6"><h2>See More</h2></div>
       </div>
+      </div>
+      <div class="sm"><h2>See More</h2></div>
   </div>
 </div>
 
