cf27040fb85da0c4baa796cf10271ee81f15ed61
dr4j <dhiraj.s.k5161@gmail.com>
2017-07-25 13:55:20 +0530

    fonts changes

--- header.php
+++ header.php
@@ -14,9 +14,10 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link href="https://fonts.googleapis.com/css?family=Francois+One|Oswald:300" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Nova+Mono|Share+Tech+Mono" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<!-- <link href="https://fonts.googleapis.com/css?family=Francois+One|Oswald:300" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Nova+Mono|Share+Tech+Mono" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">-->
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 
